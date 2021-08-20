1154070634d10abb08d2756dee2ae9fa4b2f1417
drewchen <drewchen@umich.edu>
2021-08-20 14:16:40 -0400

    Function Definition

--- stock.py
+++ stock.py
@@ -1,5 +1,6 @@
 import sqlite3
 import time
+import hashlib
 import yfinance as yf
 
 #Grab ticker information for each stock 
@@ -8,42 +9,46 @@ def get_ticker_price(self, ticker):
     return stock.info['currentPrice']
 
 """Need to fix these functions"""
-
-
-#Be able to create to tables in sqlite3
-def __init__(self):
-    self.con = sqlite3.connect("/Users/andrew/Documents/Personal Project/StockPlatform/stock.db")
-    self.cur = self.con.cursor()
     
-    #Two tables
-    """
-        1 table where we hold 50-10+
-            To_the_moon(ticker, purchase price, date)
-        1 table where we buy and sell 50-10 
-            Index_fund(ticker,been_purchased)
-                ticker_idtable(purchase, selling, date_purchase,date_sell,profit)
-                
-        1 table where we buy and sell 10 and lower stocks 
-            Penny_stocks(ticker,been_purchased)
-                table(purchase, selling, date_purchase,date_sell,profit)
+  
 
-        All stocks will be based on insider trading news 
-    """
-def create_database(self):
-    self.con = sqlite3.connect("/Users/andrew/Documents/Personal Project/StockBot/stock.db")
-    self.cur = self.con.cursor()
+def create_database():
+    con = sqlite3.connect("/Users/andrew/Documents/Personal Project/StockPlatform/stock.db")
+    return con
 
-def create_table(self):
-    self.cur.execute('''CREATE TABLE To_the_moon
+def create_table_account(con,name):
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE To_the_moon
                (ticker,purchase_price,date)''')
-    self.con.commit()
+    con.commit()
 
-def insert_data(self, datalist):
+def create_table_stocks(con,id):
+    pass
+
+def create_table_trades(con,id):
+    pass
+
+def insert_data_users(self, datalist):
         #datalist = [ticker,price,date]
     self.cur.execute("INSERT INTO To_the_moon VALUES {ticker},{price},{date})".format(ticker = datalist[0],price = datalist[1],date= datalist[2]))
     self.con.commit()
+
+def insert_data_accounts():
+    pass
+
+def insert_data_trades():
+    pass
+
+def insert_data_stocks():
+    pass
         
-    
+def remove_data_account():
+    pass
+
+def remove_data_stocks():
+    pass
+
+
 def retrieve_data(self):
     for row in self.cur.execute('SELECT * FROM To_the_moon ORDER BY purchase_price'):
         print(row)
